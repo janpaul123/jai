82c25975f610255140a27a16b2754ad6563230a8
Josh Huelsman <joshuahuelsman@gmail.com>
2016-06-03 23:05:17 -0400

    Add function call parsing. Add function call emitter, primary expression emitter.

--- driver/main.cpp
+++ driver/main.cpp
@@ -69,6 +69,8 @@ template <typename T> struct Array {
   }
 };
 
+typedef Array<char> String;
+
 enum Ast_Type {
   AST_TRANSLATION_UNIT = 1,
   AST_DECLARATION,
@@ -80,6 +82,8 @@ enum Ast_Type {
   AST_IDENTIFIER,
   AST_SELECTION_STATEMENT,
   AST_ITERATION_STATEMENT,
+  AST_FUNCTION_CALL,
+  AST_PRIMARY_EXPRESSION,
 };
 
 struct Ast {
@@ -126,14 +130,38 @@ struct Ast_Ident : public Ast {
   char *my_name;
 };
 
-struct Ast_Expression : public Ast {
+struct Ast_Statement : public Ast {
+  Ast_Statement() { type = AST_STATEMENT; }
+  Array<Ast_Statement *> compound_stmts;
+};
+
+struct Ast_Expression : public Ast_Statement {
   Ast_Expression() { type = AST_EXPRESSION; }
   Ast_Type_Info *inferred_type;
 };
 
-struct Ast_Statement : public Ast {
-  Ast_Statement() { type = AST_STATEMENT; }
-  Array <Ast_Statement *> compound_stmts;
+enum Primary_Type {
+  AST_PT_CONST_INT,
+  AST_PT_CONST_FLOAT,
+  AST_PT_STRING_LITERAL,
+  AST_PT_CONST_BOOL,
+  AST_PT_IDENTIFIER,
+};
+
+struct Ast_Primary_Expression : public Ast_Expression {
+  Ast_Primary_Expression() { type = AST_PRIMARY_EXPRESSION; }
+  Primary_Type expr_type;
+  long int_const;
+  double float_const;
+  String string_literal;
+  Ast_Ident *ident;
+  Ast_Expression *expr;
+};
+
+struct Ast_Function_Call : public Ast_Expression {
+  Ast_Function_Call() { type = AST_FUNCTION_CALL; }
+  Ast_Ident *my_ident;
+  Array<Ast_Expression *> params;
 };
 
 struct Ast_Selection_Statement : public Ast_Statement {
@@ -142,18 +170,18 @@ struct Ast_Selection_Statement : public Ast_Statement {
   Ast_Expression *expr;
 };
 
-struct Ast_Iteration_Statement : public Ast_Statement {
-  Ast_Iteration_Statement() { type = AST_ITERATION_STATEMENT; }
-  Ast_Declaration *decl;
-  Ast_Expression *expr;
-  Ast_Expression *cond;
-};
-
 struct Ast_Declaration : public Ast_Statement {
   Ast_Declaration() { type = AST_DECLARATION; }
   Ast_Type_Info *my_type;
   Ast_Ident *my_ident;
   Ast_Expression *expr;
+};
+
+struct Ast_Iteration_Statement : public Ast_Statement {
+  Ast_Iteration_Statement() { type = AST_ITERATION_STATEMENT; }
+  Ast_Declaration *decl;
+  Ast_Expression *expr;
+  Ast_Expression *cond;
 };
 
 struct Ast_Lambda : public Ast_Declaration {
@@ -173,6 +201,7 @@ struct Jai_Parser {
   char *filename;
 
   void match_token(int type);
+  Ast_Primary_Expression *parse_primary_expression();
   Ast_Statement *parse_statement();
   Ast_Type_Info *parse_type();
   Ast_Lambda *parse_lambda_prototype();
@@ -243,8 +272,61 @@ Ast_Lambda *Jai_Parser::parse_lambda_prototype() {
   return lamda;
 }
 
-Ast_Expression *Jai_Parser::parse_expression() { return nullptr; }
+Ast_Primary_Expression *Jai_Parser::parse_primary_expression() {
+  auto pe = AST_NEW(Ast_Primary_Expression);
+  switch (tok.Type) {
+  case token::INTCONSTANT:
+    pe->expr_type = AST_PT_CONST_INT;
+    pe->int_const = tok.IntValue;
+    match_token(tok.Type);
+    return pe;
+  case token::FLOATCONSTANT:
+    pe->expr_type = AST_PT_CONST_FLOAT;
+    pe->float_const = tok.FloatValue;
+    match_token(tok.Type);
+    return pe;
+  case token::BOOLCONSTANT:
+    pe->expr_type = AST_PT_CONST_BOOL;
+    pe->int_const = tok.BoolValue;
+    match_token(tok.Type);
+    return pe;
+  case token::DQSTRING: {
+    pe->expr_type = AST_PT_STRING_LITERAL;
+    char *str = (char *)calloc(0, tok.Id.length() + 1);
+    memcpy(str, tok.Id.c_str(), tok.Id.length());
+    pe->string_literal.array = str;
+    pe->string_literal.count = tok.Id.length();
+    pe->string_literal.array_size = tok.Id.length() + 1;
+    match_token(tok.Type);
+    return pe;
+  }
+  case token::IDENTIFIER:
+    pe->expr_type = AST_PT_IDENTIFIER;
+    pe->ident = parse_ident();
+    match_token(tok.Type);
+    return pe;
+  }
 
+  return nullptr;
+}
+
+Ast_Expression *Jai_Parser::parse_expression() {
+  if (tok.Type == token::IDENTIFIER &&
+      lex->PeekToken().Type == token::LEFT_PAREN) {
+    Ast_Function_Call *fc = AST_NEW(Ast_Function_Call);
+    fc->my_ident = parse_ident();
+    match_token(token::LEFT_PAREN);
+    while (tok.Type != token::RIGHT_PAREN) {
+      fc->params.push(parse_expression());
+      if (tok.Type != token::COMMA)
+        break;
+    }
+    match_token(token::RIGHT_PAREN);
+    return fc;
+  }
+
+  return parse_primary_expression();
+}
 
 Ast_Statement *Jai_Parser::parse_statement() {
   if (tok.Type == token::IF) {
@@ -260,10 +342,11 @@ Ast_Statement *Jai_Parser::parse_statement() {
   } else if (tok.Type == token::FOR) {
 
   } else {
-
   }
 
-  return nullptr;
+  auto expr = parse_expression();
+  match_token(token::SEMICOLON);
+  return expr;
 }
 
 Ast_Ident *Jai_Parser::parse_ident() {
@@ -325,29 +408,86 @@ Ast_Translation_Unit *Jai_Parser::parse_translation_unit(lexer_state *L) {
 }
 
 struct C_Converter {
+  static void emit_expression(Ast_Expression *expr, std::ostream &os);
   static void emit_statement(Ast_Statement *stmt, std::ostream &os);
   static void emit_type_info(Ast_Type_Info *info, std::ostream &os);
   static void emit_ident(Ast_Ident *ident, std::ostream &os);
   static void emit_translation_unit(Ast_Translation_Unit *tu, std::ostream &os);
 };
 
-void C_Converter::emit_statement(Ast_Statement *stmt, std::ostream &os) {}
+void C_Converter::emit_expression(Ast_Expression *expr, std::ostream &os) {
+  if (expr->type == AST_PRIMARY_EXPRESSION) {
+    auto pe = static_cast<Ast_Primary_Expression *>(expr);
+    switch (pe->expr_type) {
+    case AST_PT_CONST_INT:
+      os << pe->int_const;
+      break;
+    case AST_PT_CONST_BOOL:
+      os << (pe->int_const ? "true" : "false");
+      break;
+    case AST_PT_CONST_FLOAT:
+      os << pe->float_const;
+      break;
+    case AST_PT_STRING_LITERAL:
+      os << "\"";
+      for (int i = 0; i < pe->string_literal.count; ++i) {
+        switch (pe->string_literal.array[i]) {
+        case '\n':
+          os << "\\n";
+          break;
+        case '\r':
+          os << "\\r";
+          break;
+        case '\t':
+          os << "\\t";
+          break;
+        case '\'':
+          os << "\\'";
+          break;
+        default:
+          os << pe->string_literal.array[i];
+        }
+      }
+      os << "\"";
+      break;
+    case AST_PT_IDENTIFIER:
+      os << pe->ident->my_name;
+      break;
+    }
+  }
+}
+
+void C_Converter::emit_statement(Ast_Statement *stmt, std::ostream &os) {
+  if (stmt->type == AST_FUNCTION_CALL) {
+    auto fc = static_cast<Ast_Function_Call *>(stmt);
+    os << fc->my_ident->my_name << "(";
+    for (int i = 0; i < fc->params.count; ++i) {
+      auto param = fc->params.array[i];
+      emit_expression(param, os);
+      if (i < fc->params.count - 1) {
+        os << ", ";
+      }
+    }
+    os << ")"
+       << ";" << std::endl;
+  }
+}
 
 void C_Converter::emit_type_info(Ast_Type_Info *info, std::ostream &os) {
   switch (info->atom_type) {
-    case AST_TYPE_VOID:
+  case AST_TYPE_VOID:
     os << "void";
     break;
-    case AST_TYPE_INT:
+  case AST_TYPE_INT:
     os << "s32";
     break;
-    case AST_TYPE_BOOL:
+  case AST_TYPE_BOOL:
     os << "bool";
     break;
-    case AST_TYPE_FLOAT:
+  case AST_TYPE_FLOAT:
     os << "float32";
     break;
-    case AST_TYPE_TYPENAME:
+  case AST_TYPE_TYPENAME:
     // lookup
     break;
   }
